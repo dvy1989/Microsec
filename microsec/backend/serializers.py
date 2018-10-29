c9c5c36131ae6327df65f0030f6c5b379e498429
dvy1989 <dobrodeevvy@gmail.com>
2018-10-29 22:24:57 +0200

    Each uqestion is assigned a separate folder

--- microsec/backend/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from .models import Sensor
-
-
-class SensorSerializer(ModelSerializer):
-    class Meta:
-        model = Sensor
-        fields = ('name',)
