422c89dd553051d49064f1b274ba1539de088501
dvy1989 <dobrodeevvy@gmail.com>
2018-10-29 23:00:09 +0200

    Question2 fixing shell script running at service side

--- Question2/server.py
+++ Question2/server.py
@@ -1,14 +1,14 @@
 from os.path import join
 from socketserver import ThreadingMixIn, TCPServer, BaseRequestHandler
-from subprocess import Popen, PIPE
+from subprocess import run, PIPE
 
 
 class ThreadedTCPRequestHandler(BaseRequestHandler):
     def handle(self):
-        command = ["./script.sh"] + self.request.recv(1024).decode().split(" ")
+        command = ['./script.sh'] + self.request.recv(1024).decode().split(' ')
         print(command)
-        process = Popen(command, stdout=PIPE, stderr=PIPE, cwd=join("..", "Question1"))
-        while process.poll() is None:
+        process = run(command, stdout=PIPE, cwd=join('..', 'Question1'))
+        while True:
             for line in process.stdout:
                 self.request.sendall(line)
 
@@ -17,7 +17,7 @@ class ThreadedTCPServer(ThreadingMixIn, TCPServer):
     pass
 
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 2999
+if __name__ == '__main__':
+    HOST, PORT = 'localhost', 2999
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     server.serve_forever()
