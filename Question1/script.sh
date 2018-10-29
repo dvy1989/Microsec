6b01f88ff228cd0699d88a2b6a5a334fe54eda27
dvy1989 <dobrodeevvy@gmail.com>
2018-10-29 22:14:26 +0200

    Question1 is done

--- Question1/script.sh
+++ Question1/script.sh
@@ -1,4 +1,19 @@
-# get word occurencies
-wget -q -O - $1 | grep -oP ".{0,10}$2.{0,10}"
-# Get hyperlinks
-wget -q -O - $1 | grep -oP '(?<=<a).*(?=(<\/a>)) | grep -oP '(?<=href=").*(?=">)' | cut -d ' ' -f1 | grep -oP '.*(?=")'
+if [ -f visited ]; then
+   if grep -Fxq $1 visited; then
+      exit 0
+   fi
+fi
+found=$(wget -q -O - $1 | grep -oP ".{0,10}$2.{0,10}")
+if [ ! found ]; then
+   exit 0
+fi
+for str in $found; do
+   echo "$1;$str"
+done
+echo $1 >> visited
+links=$(wget -q -O - $1 | grep -oP '(?<=<a).*(?=(<\/a>))' | grep -oP '(?<=href=").*(?=>)' | cut -d ' ' -f1 | cut -d '"' -f1 | sed 's/^\/\/\(.*\)/\1/')
+for link in $links; do
+  if [[ ${link:0:1} != \# ]]; then
+     ./script1.sh $link $2
+  fi
+done
